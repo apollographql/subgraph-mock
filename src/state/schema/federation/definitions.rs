185a54a740b27accae32e4c18e94a453d883527e
Isaac M. Good <imgood@apollographql.com>
2026-01-21 08:23:15 -0800

    Break out definition boilerplate into a separate module

--- src/state/schema/federation/definitions.rs
+++ src/state/schema/federation/definitions.rs
@@ -1,269 +1,18 @@
-use anyhow::anyhow;
+use super::FederationType;
 use apollo_compiler::{
-    Name, Node, Schema,
-    ast::{
-        Definition, Directive, DirectiveList, Document, EnumValueDefinition, FieldDefinition,
-        InputValueDefinition, OperationType, SchemaDefinition, Type,
-    },
-    collections::IndexSet,
+    Node, Schema,
+    ast::{Definition, EnumValueDefinition, FieldDefinition, InputValueDefinition, Type},
     name,
     schema::{
-        Component, ComponentName, ComponentOrigin, DirectiveDefinition, DirectiveLocation,
-        EnumType, ExtendedType, ObjectType, ScalarType, UnionType,
+        Component, DirectiveDefinition, DirectiveLocation, EnumType, ExtendedType, ObjectType,
+        ScalarType,
     },
     ty,
 };
-use tracing::warn;
 
-/// We need to be able to intercept and handle queries for entities and service:
-/// {
-///   _entities(representations: [_Any!]!): [_Entity]!
-///   _service: _Service!
-/// }
-///
-/// The router also auto-supports the @defer and @stream directive so schemas may be using them without
-/// importing / defining them directly. In that case we need to inject them into the schema in
-/// order for the validation of our queries to succeed.
-///
-/// See https://www.apollographql.com/docs/graphos/routing/operations/defer
-///
-/// The directive definitions are copied from here:
-///   https://github.com/apollographql/router/blob/23e580e22a4401cc2e7a952b241a1ec955b29c99/apollo-federation/src/api_schema.rs#L156https://github.com/apollographql/router/blob/23e580e22a4401cc2e7a952b241a1ec955b29c99/apollo-federation/src/api_schema.rs#L156
-pub fn patch_schema(schema: &mut Schema, federation_type: FederationType) -> anyhow::Result<()> {
-    // Resolve federated types for the _Entity union.
-    let members: IndexSet<ComponentName> = schema
-        .types
-        .iter()
-        .filter(|(_, ty)| is_federated_type(schema, ty))
-        .map(|(name, _)| ComponentName {
-            origin: ComponentOrigin::Definition,
-            name: name.clone(),
-        })
-        .collect();
-
-    let has_federated_members = !members.is_empty();
-    if has_federated_members {
-        // Inject our _Entity union
-        schema.types.insert(
-            name!("_Entity"),
-            ExtendedType::Union(Node::new(UnionType {
-                description: None,
-                name: name!("_Entity"),
-                directives: Default::default(),
-                members,
-            })),
-        );
-    }
-    // Inject all other federation types that aren't dynamic
-    insert_federation_types(schema, &federation_type);
-
-    let query_type_name = if let FederationType::Subgraph = federation_type {
-        // Create the Query type if it doesn't exist, which Federation-compatible schemas won't always do
-        if schema.schema_definition.query.is_none() {
-            schema.schema_definition.make_mut().query = Some(name!("Query").into());
-            schema.types.insert(
-                name!("Query"),
-                Node::new(ObjectType {
-                    description: None,
-                    name: name!("Query"),
-                    implements_interfaces: Default::default(),
-                    directives: Default::default(),
-                    fields: Default::default(),
-                })
-                .into(),
-            );
-        }
-        let query_type_name: &Name = schema.schema_definition.query.as_ref().unwrap();
-        if !schema.types.contains_key(query_type_name) {
-            schema.types.insert(
-                query_type_name.clone(),
-                Node::new(ObjectType {
-                    description: None,
-                    name: query_type_name.clone(),
-                    implements_interfaces: Default::default(),
-                    directives: Default::default(),
-                    fields: Default::default(),
-                })
-                .into(),
-            );
-        }
-        query_type_name
-    } else {
-        schema
-            .schema_definition
-            .query
-            .as_ref()
-            .ok_or_else(|| anyhow!("Schema does not define a query type"))?
-    };
-
-    // Inject _entities query if appropriate and the _service query
-    let query_root = match schema.types.get_mut(query_type_name).unwrap() {
-        ExtendedType::Object(obj) => obj,
-        _ => return Err(anyhow!("query root is not an object")),
-    };
-
-    if has_federated_members {
-        query_root.make_mut().fields.insert(
-            name!("_entities"),
-            Component::new(FieldDefinition {
-                description: None,
-                name: name!("_entities"),
-                arguments: vec![Node::new(InputValueDefinition {
-                    description: None,
-                    name: name!("representations"),
-                    ty: Node::new(Type::NonNullList(Box::new(Type::NonNullNamed(name!(
-                        "_Any"
-                    ))))),
-                    default_value: None,
-                    directives: Default::default(),
-                })],
-                ty: Type::NonNullList(Box::new(Type::Named(name!("_Entity")))),
-                directives: Default::default(),
-            }),
-        );
-    }
-
-    query_root.make_mut().fields.insert(
-        name!("_service"),
-        Component::new(FieldDefinition {
-            description: None,
-            name: name!("_service"),
-            arguments: vec![],
-            ty: Type::NonNullNamed(name!("_Service")),
-            directives: Default::default(),
-        }),
-    );
-
-    if let FederationType::Supergraph = federation_type {
-        // Matching the behaviour in the Router:
-        //   https://github.com/apollographql/router/blob/23e580e22a4401cc2e7a952b241a1ec955b29c99/apollo-federation/src/api_schema.rs#L139-L149
-        // These are not yet formally defined in the Federation/GraphQL spec, so we will match the software-injected behavior
-        // rather than pulling in the literal GraphQL definitions.
-        if !schema.directive_definitions.contains_key(&name!("defer")) {
-            schema
-                .directive_definitions
-                .insert(name!("defer"), defer_definition());
-        }
-        if !schema.directive_definitions.contains_key(&name!("stream")) {
-            schema
-                .directive_definitions
-                .insert(name!("stream"), stream_definition());
-        }
-    }
-
-    Ok(())
-}
-
-#[derive(Debug)]
-pub enum FederationType {
-    Subgraph,
-    Supergraph,
-    None,
-}
-
-/// Federated schemas do not start out as valid GraphQL schemas and must be patched before they will parse as one.
-///
-/// This means patching in a schema definition if it doesn't exist, and ensuring all relevant directives are in scope.
-///
-/// Returns the federation type of this schema as inferred by the presence of key directives and types.
-/// Subgraph schemas are identified by the presence of the `@link` directive on their schema extension.
-/// Supergraph schemas are identified by the presence of the `join__Graph` enum.
-pub fn patch_ast(ast: &mut Document) -> FederationType {
-    let schema_extension = ast.definitions.iter().find_map(|def| match def {
-        Definition::SchemaExtension(node) => Some(node),
-        _ => None,
-    });
-
-    // The `join__Graph` enum is a required part of the supergraph spec
-    let fed_type = if ast
-        .definitions
-        .iter()
-        .any(|definion| definion.name().is_some_and(|name| name == "join__Graph"))
-    {
-        FederationType::Supergraph
-    } else if let Some(extension) = schema_extension
-        // If `join__Graph` is not present, but the schema is still extended with `@link`, then this is a subgraph schema
-        && extension.directives.iter().any(|dir| dir.name == "link")
-    {
-        // Federated subgraph schemas can omit the root schema definition entirely, and it is expected to be implicitly added
-        if !ast
-            .definitions
-            .iter()
-            .any(|def| matches!(def, Definition::SchemaDefinition(_)))
-        {
-            ast.definitions
-                .push(Definition::SchemaDefinition(Node::new(SchemaDefinition {
-                    description: None,
-                    directives: Default::default(),
-                    root_operations: vec![Node::new((OperationType::Query, name!("Query")))],
-                })));
-        }
-        // The federation spec requires that all these directives be implicitly added to the schema for a subgraph server
-        ast.definitions.append(&mut federation_directives());
-
-        FederationType::Subgraph
-    } else {
-        FederationType::None
-    };
-
-    if let FederationType::Subgraph | FederationType::Supergraph = fed_type {
-        // The `@link` directive must be followed to import values that may be referenced in the file
-        // This behavior is currently not implemented.
-        for def in &ast.definitions {
-            if let Definition::SchemaDefinition(schema_def) = def {
-                process_link_directives(&schema_def.directives);
-            }
-            if let Definition::SchemaExtension(schema_ext) = def {
-                process_link_directives(&schema_ext.directives);
-            }
-        }
-    }
-
-    fed_type
-}
-
-/// Determines if a type is federated based on its schema definition
-fn is_federated_type(schema: &Schema, ty: &ExtendedType) -> bool {
-    ty.directives().iter().any(|directive| {
-        is_federated_directive(schema, directive)
-            // Do not include the query type if it is defined
-            && schema
-                .schema_definition
-                .query
-                .as_ref()
-                .is_none_or(|query| &query.name != ty.name())
-    })
-}
-
-/// Determines if a directive represents a federated type
-///
-/// If we are loading a supergraph schema, types that are federated will use `@join__type`.
-/// If we are loading a subgraph schema, types that are federated will use [`@key`](key_definition).
-fn is_federated_directive(schema: &Schema, directive: &Component<Directive>) -> bool {
-    match directive.name.as_str() {
-        "key" | "join__type" => {
-            // federated unless explicitly marked resolvable: false
-            directive
-                .argument_by_name("resolvable", schema)
-                .ok()
-                .and_then(|arg| arg.to_bool())
-                .expect("the @key and @join__type directives specify 'resolvable' as a boolean argument")
-        }
-        _ => false,
-    }
-}
-
-/// `@link` directives describe external imports of other GraphQL schema files. We do not currently handle
-/// that external resolution as our present use cases only are "importing" the federation spec itself.
-fn process_link_directives(directives: &DirectiveList) {
-    for directive in directives {
-        if directive.name == "link" {
-            warn!("@link directive detected, but link directive resolution is not implemented.")
-        }
-    }
-}
-
-fn federation_directives() -> Vec<Definition> {
+/// Directives that must be implicitly injected into federated schemas:
+/// https://www.apollographql.com/docs/graphos/schema-design/federated-schemas/reference/subgraph-spec#subgraph-schema-additions
+pub fn federation_directives() -> Vec<Definition> {
     vec![
         external_definition(),
         requires_definition(),
@@ -286,7 +35,7 @@ fn federation_directives() -> Vec<Definition> {
 
 /// Types that must be implicitly injected into federated schemas:
 /// https://www.apollographql.com/docs/graphos/schema-design/federated-schemas/reference/subgraph-spec#subgraph-schema-additions
-fn insert_federation_types(schema: &mut Schema, federation_type: &FederationType) {
+pub fn insert_federation_types(schema: &mut Schema, federation_type: &FederationType) {
     // We always support the _service federated query even if the schema is unfederated
     schema.types.insert(
         name!("_Service"),
@@ -396,7 +145,7 @@ fn insert_federation_types(schema: &mut Schema, federation_type: &FederationType
     }
 }
 
-fn link_definition() -> Definition {
+pub fn link_definition() -> Definition {
     Definition::DirectiveDefinition(Node::new(DirectiveDefinition {
         description: None,
         name: name!("link"),
@@ -435,7 +184,7 @@ fn link_definition() -> Definition {
     }))
 }
 
-fn key_definition() -> Definition {
+pub fn key_definition() -> Definition {
     Definition::DirectiveDefinition(Node::new(DirectiveDefinition {
         description: None,
         name: name!("key"),
@@ -460,7 +209,7 @@ fn key_definition() -> Definition {
     }))
 }
 
-fn requires_definition() -> Definition {
+pub fn requires_definition() -> Definition {
     Definition::DirectiveDefinition(Node::new(DirectiveDefinition {
         description: None,
         name: name!("requires"),
@@ -476,7 +225,7 @@ fn requires_definition() -> Definition {
     }))
 }
 
-fn provides_definition() -> Definition {
+pub fn provides_definition() -> Definition {
     Definition::DirectiveDefinition(Node::new(DirectiveDefinition {
         description: None,
         name: name!("provides"),
@@ -492,7 +241,7 @@ fn provides_definition() -> Definition {
     }))
 }
 
-fn external_definition() -> Definition {
+pub fn external_definition() -> Definition {
     Definition::DirectiveDefinition(Node::new(DirectiveDefinition {
         description: None,
         name: name!("external"),
@@ -505,7 +254,7 @@ fn external_definition() -> Definition {
     }))
 }
 
-fn tag_definition() -> Definition {
+pub fn tag_definition() -> Definition {
     Definition::DirectiveDefinition(Node::new(DirectiveDefinition {
         description: None,
         name: name!("tag"),
@@ -532,7 +281,7 @@ fn tag_definition() -> Definition {
     }))
 }
 
-fn shareable_definition() -> Definition {
+pub fn shareable_definition() -> Definition {
     Definition::DirectiveDefinition(Node::new(DirectiveDefinition {
         description: None,
         name: name!("shareable"),
@@ -545,7 +294,7 @@ fn shareable_definition() -> Definition {
     }))
 }
 
-fn inaccessible_definition() -> Definition {
+pub fn inaccessible_definition() -> Definition {
     Definition::DirectiveDefinition(Node::new(DirectiveDefinition {
         description: None,
         name: name!("inaccessible"),
@@ -566,7 +315,7 @@ fn inaccessible_definition() -> Definition {
     }))
 }
 
-fn override_definition() -> Definition {
+pub fn override_definition() -> Definition {
     Definition::DirectiveDefinition(Node::new(DirectiveDefinition {
         description: None,
         name: name!("override"),
@@ -582,7 +331,7 @@ fn override_definition() -> Definition {
     }))
 }
 
-fn compose_directive_definition() -> Definition {
+pub fn compose_directive_definition() -> Definition {
     Definition::DirectiveDefinition(Node::new(DirectiveDefinition {
         description: None,
         name: name!("composeDirective"),
@@ -598,7 +347,7 @@ fn compose_directive_definition() -> Definition {
     }))
 }
 
-fn interface_object_definition() -> Definition {
+pub fn interface_object_definition() -> Definition {
     Definition::DirectiveDefinition(Node::new(DirectiveDefinition {
         description: None,
         name: name!("interfaceObject"),
@@ -608,7 +357,7 @@ fn interface_object_definition() -> Definition {
     }))
 }
 
-fn authenticated_definition() -> Definition {
+pub fn authenticated_definition() -> Definition {
     Definition::DirectiveDefinition(Node::new(DirectiveDefinition {
         description: None,
         name: name!("authenticated"),
@@ -624,7 +373,7 @@ fn authenticated_definition() -> Definition {
     }))
 }
 
-fn requires_scopes_definition() -> Definition {
+pub fn requires_scopes_definition() -> Definition {
     Definition::DirectiveDefinition(Node::new(DirectiveDefinition {
         description: None,
         name: name!("requiresScopes"),
@@ -648,7 +397,7 @@ fn requires_scopes_definition() -> Definition {
     }))
 }
 
-fn policy_definition() -> Definition {
+pub fn policy_definition() -> Definition {
     Definition::DirectiveDefinition(Node::new(DirectiveDefinition {
         description: None,
         name: name!("policy"),
@@ -672,7 +421,7 @@ fn policy_definition() -> Definition {
     }))
 }
 
-fn context_definition() -> Definition {
+pub fn context_definition() -> Definition {
     Definition::DirectiveDefinition(Node::new(DirectiveDefinition {
         description: None,
         name: name!("context"),
@@ -692,7 +441,7 @@ fn context_definition() -> Definition {
     }))
 }
 
-fn from_context_definition() -> Definition {
+pub fn from_context_definition() -> Definition {
     Definition::DirectiveDefinition(Node::new(DirectiveDefinition {
         description: None,
         name: name!("fromContext"),
@@ -708,7 +457,7 @@ fn from_context_definition() -> Definition {
     }))
 }
 
-fn defer_definition() -> Node<DirectiveDefinition> {
+pub fn defer_definition() -> Node<DirectiveDefinition> {
     Node::new(DirectiveDefinition {
         description: None,
         name: name!("defer"),
@@ -736,7 +485,7 @@ fn defer_definition() -> Node<DirectiveDefinition> {
     })
 }
 
-fn stream_definition() -> Node<DirectiveDefinition> {
+pub fn stream_definition() -> Node<DirectiveDefinition> {
     Node::new(DirectiveDefinition {
         description: None,
         name: name!("stream"),
